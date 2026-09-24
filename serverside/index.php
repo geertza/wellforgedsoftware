0322011e833e7ae40a49f9c605a064bac60d80ec
geertza <geertza@gmail.com>
2026-09-24 10:53:26 -0600

    test router

--- serverside/index.php
+++ serverside/index.php
@@ -1,11 +1,40 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Andy's MSU Client Side</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+
 <body>
-    test test
+  <div class="navbar nav bg-dark">
+    <ul class="nav">
+      <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+      <li class="nav-item"><a class="nav-link" href="#dice">Dice Games</a></li>
+      <li class="nav-item"><a class="nav-link" href="#dnd">dnd</a></li>
+      <li class="nav-item"><a class="nav-link" href="#Cards">Cards</a></li>
+      <li class="nav-item"><a class="nav-link" href="#Projects">Projects</a></li>
+      <li class="nav-item"><a class="nav-link" href="#groceryList">Grocery List</a></li>
+      <li class="nav-item"><a class="nav-link" href="./anime/index.html">AnimaniacClub</a></li>
+    </ul>
+  </div>
+  <div id="app" class="container">
+
+  </div>
+  <script src="../js/route.js"></script>
+  <script src="../js/router.js"></script>
+  <script src="../js/app.js"></script>
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"
+  ></script>
 </body>
+
 </html>
